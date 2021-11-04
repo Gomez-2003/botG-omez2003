c6cf844e7174f52cd97b352c8f60805f21fc715a
Gómez <89650548+Gomez-2003@users.noreply.github.com>
2021-11-04 05:01:08 -0500

    𝕋𝔸𝕋𝕀𝕊 𝕐 𝔾𝕆𝕄𝔼ℤ
    𝕋𝔸𝕋𝕀𝕊 𝕐 𝔾𝕆𝕄𝔼ℤ
    (\_/)
    ( •_•)
    />❤‍🔥

---
+++ Lib/redes.js
@@ -0,0 +1,25 @@
+const ytchoute = 'https://www.youtube.com/channel/UC-HPutaDGeTPjrCId0bXQgg'
+const gitchoute = 'github.com/Thechoute'
+const instachoute = '@the_choute_'
+const whachoute = 'wa.me/18299897014'
+const gpwha = 'https://chat.whatsapp.com/'
+
+module.exports = {
+     ytchoute, 
+    gitchoute,
+instachoute.
+whachoute,
+    gpwha, }
+
+
+
+
+    ytchoute, 
+
+    gitchoute,
+
+instachoute,
+
+whachoute,
+
+gpwha,
