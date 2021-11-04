755582aedc9e79816399f80f6833d7ad3590c410
Gómez <89650548+Gomez-2003@users.noreply.github.com>
2021-11-04 05:51:25 -0500

    𝕋𝔸𝕋𝕀𝕊 𝕐 𝔾𝕆𝕄𝔼ℤ
    𝕋𝔸𝕋𝕀𝕊 𝕐 𝔾𝕆𝕄𝔼ℤ
    (\_/)
    ( •_•)
    />❤‍🔥

---
+++ Lib/exportaciones.js/bot.js
@@ -0,0 +1,19 @@
+const bot = 
+
+`
+
+Sigueme en mis redes sociales, 
+
+*Instagram:* no te preocupes pronto estara disponible
+
+
+
+*Youtube:* https://no te preocupes pronto estara disponible
+
+Y envia capture/screenshot al creador`
+
+module.exports = {
+
+    bot
+
+}
